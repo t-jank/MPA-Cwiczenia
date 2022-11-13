f5aa855fe6613ec58c0186452bf4ff3fdb8ba43b
TomaszJankowiak <62778369+TomaszJankowiak@users.noreply.github.com>
2022-11-13 11:42:01 +0100

    herezje removed

--- zad.15_malpa.py
+++ zad.15_malpa.py
@@ -12,16 +12,14 @@ dopodobieństwem. Jeśli małpa napisała 1000000 liter, jaka jest wartość ocz
 wana liczby wystąpień słowa "dowod"?
 '''
 '''
-Analitycznie: byloby 999996 / 26^5 = 0.084, ale po 'dowod' jest wieksza szansa
-ze znowu bedzie, bo juz jedna literka jest i nie bedzie zdarzenie niezalezne;
-wieksza szansa bedzie, ok 0.095 chyba jak wyszlo z symulacji
+Analitycznie: 999996 / 26^5 = 0.084
 '''
 
 import random
 
 alfabet=('q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m')
 
-repeat_number = 500
+repeat_number = 100
 number_of_letters = 1000000
 suma = 0
 
@@ -41,3 +39,31 @@ for rn in range(0,repeat_number):
     suma += len(res)
 
 print("Średnia liczba wystąpień słowa 'dowod' w",number_of_letters,"liter:", suma/repeat_number)
+
+
+
+'''
+############# badanie na mniejszych danych, aby zweryfikowac myslenie
+
+alfabet = ('d','e','f','o')
+perm=[]
+for i in range (0,len(alfabet)):
+    for j in range(0,len(alfabet)):
+        for k in range(0,len(alfabet)):
+            for l in range(0,len(alfabet)):
+                for m in range(0,len(alfabet)):
+                    for n in range(0,len(alfabet)):
+                        for o in range(0,len(alfabet)):
+                            perm.append(alfabet[i]+alfabet[j]+alfabet[k]+alfabet[l]+alfabet[m]+alfabet[n]+alfabet[o])
+
+suma=0
+for u in range(0,len(perm)):
+    napis=perm[u]
+    res = [i for i in range(len(napis)) if napis.startswith('dod', i)]
+    if len(res)>1:
+        print('O!',len(res))
+    suma += len(res)
+print(suma)
+print(len(perm))
+print(suma/len(perm))
+'''
